44787a5cf3ad60b7f1e64ef3075909de6d6c0136
mszgajewski <mszgajewski@gmail.com>
2021-10-26 14:12:20 +0200

    Initial commit

---
+++ src/main/java/hyperskill/java/JavaApplication.java
@@ -0,0 +1,15 @@
+package hyperskill.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaApplication.class, args);
+	}
+
+}
+
+
